65e4a6cc36223a8ac4c6a6e77f40ba7babe7f375
Josue Martinez Eriksson <josueme1992@hotmail.es>
2017-04-05 18:00:23 +0200

    Crea la carpeta demo

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '8705a0750adca0bd9803104d8130c8aa3fc733a09a0c3a0db1e2efeafaaf3f1314078e271f0954b929ba018d5775c162f62d86c7ec02d6788eb65ef2d838ba08'
