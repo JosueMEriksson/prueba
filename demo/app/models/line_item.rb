8a394e79fa4fe8a4f99a652c4538f8070277d3c2
Josue Martinez Eriksson <josueme1992@hotmail.es>
2017-04-21 11:48:01 +0200

    Termina la implementacion del carrito

--- demo/app/models/line_item.rb
+++ demo/app/models/line_item.rb
@@ -1,6 +1,9 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :product, :cart, :product_id, :cart_id
-  belongs_to :product
-  belongs_to :cart
+	attr_accessible :product, :cart, :product_id, :cart_id, :quantity
+	belongs_to :product
+	belongs_to :cart
 
+	def total_price
+		product.price * quantity
+	end
 end
